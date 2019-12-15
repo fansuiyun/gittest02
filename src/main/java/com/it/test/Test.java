3343e596c570755e3c18d0a656a8a7b2a23b7433
fansuiyun <530625176@qq.com>
2019-12-15 21:35:40 +0800

    分支a2

--- src/main/java/com/it/test/Test.java
+++ src/main/java/com/it/test/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 
     public static void main(String[] args){
-
+        String name = "a2";
+        String age = "13";
         System.out.println("pppppp");
     }
 
--- src/main/java/com/it/test/Test2.java
+++ src/main/java/com/it/test/Test2.java
@@ -4,6 +4,8 @@ public class Test2 {
 
     public void test(){
 
+        String name = "a2";
+        String age = "13";
         System.out.println("a111111111");
 
     }
