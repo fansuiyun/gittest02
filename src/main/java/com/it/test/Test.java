36ad8408d23fe1ea2793cff5429df48b283ea2b0
fansuiyun <530625176@qq.com>
2019-12-15 21:34:17 +0800

    分支a1

--- src/main/java/com/it/test/Test.java
+++ src/main/java/com/it/test/Test.java
@@ -5,6 +5,8 @@ public class Test {
 
     public static void main(String[] args){
 
+        String name = "a1";
+        int age = 12;
         System.out.println("pppppp");
     }
 
--- src/main/java/com/it/test/Test2.java
+++ src/main/java/com/it/test/Test2.java
@@ -3,7 +3,8 @@ package com.it.test;
 public class Test2 {
 
     public void test(){
-
+        String name = "a1";
+        int age = 12;
         System.out.println("a111111111");
 
     }
